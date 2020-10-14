06ee2d741356df6709da1b5f55c6747975abcdf2
dimasmaspur <dimasmaspur@gmail.com>
2020-10-15 04:08:08 +0700

    implement jwt

--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mediaHandler = require('./handler/media');
+const verifyToken = require('../middlewares/verifyToken');
 
 
 /* GET users listing. */
-router.get('/', mediaHandler.getAll);
+router.get('/', verifyToken, mediaHandler.getAll);
 router.post('/', mediaHandler.create);
 router.delete('/:id', mediaHandler.destroy);
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { APP_NAME } = process.env;
+const userHandler = require('./handler/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(APP_NAME);
-});
+
+router.post('/register', userHandler.register);
+router.post('/login', userHandler.login);
 
 module.exports = router;
