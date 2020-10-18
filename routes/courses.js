53c3e45a014734a8f8c994c6f0430b96e93d9368
dimasmaspur <dimasmaspur@gmail.com>
2020-10-19 04:20:22 +0700

    update

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { APP_NAME } = process.env;
+const courseHandler = require('./handler/courses');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(APP_NAME);
-});
+const verifyToken = require('../middlewares/verifyToken');
+
+
+router.get('/', courseHandler.getAll);
+router.get('/:id', courseHandler.get);
+router.post('/',verifyToken, courseHandler.create);
+router.put('/:id',verifyToken, courseHandler.update);
+router.delete('/:id',verifyToken, courseHandler.destroy);
 
 module.exports = router;
