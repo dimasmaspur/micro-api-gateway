e90372f51325d184f49b5bca95b9b02a4eaf81ce
dimasmaspur <dimasmaspur@gmail.com>
2020-11-01 23:41:50 +0700

    revisi di app.js

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const coursesRouter = require('./routes/courses');
-const mediaRouter = require('./routes/media');
-const refreshTokenRouter = require('./routes/refreshTokens');
-const mentorsRouter = require('./routes/mentors');
-const chaptersRouter = require('./routes/chapters');
-const lessonsRouter = require('./routes/lessons');
-const imageCoursesRouter = require('./routes/imageCourses');
-const myCoursesRouter = require('./routes/myCourses');
-const reviewsRouter = require('./routes/reviews');
-const webhookRouter = require('./routes/webhook');
-const orderPaymentRouter = require('./routes/ordersPayment');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const coursesRouter = require("./routes/courses");
+const mediaRouter = require("./routes/media");
+const refreshTokenRouter = require("./routes/refreshTokens");
+const mentorsRouter = require("./routes/mentors");
+const chaptersRouter = require("./routes/chapters");
+const lessonsRouter = require("./routes/lessons");
+const imageCoursesRouter = require("./routes/imageCourses");
+const myCoursesRouter = require("./routes/myCourses");
+const reviewsRouter = require("./routes/reviews");
+const webhookRouter = require("./routes/webhook");
+const orderPaymentRouter = require("./routes/ordersPayment");
 
-const verifyToken = require('./middlewares/verifyToken');
-const can = require('./middlewares/permission');
+const verifyToken = require("./middlewares/verifyToken");
+const can = require("./middlewares/permission");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json({limit:'50mb'}));
-app.use(express.urlencoded({ extended: false , limit:'50mb'}));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
-app.use('/media', verifyToken, can('admin'), can('student'), mediaRouter);
-app.use('/refresh-tokens', refreshTokenRouter);
-app.use('/mentors', verifyToken, can('admin'), mentorsRouter);
-app.use('/chapters',verifyToken, can('admin'), chaptersRouter);
-app.use('/lessons',verifyToken, can('admin'), lessonsRouter);
-app.use('/image-courses',verifyToken, can('admin'), imageCoursesRouter);
-app.use('/my-courses',verifyToken, can('admin'), can('student'), myCoursesRouter);
-app.use('/reviews',verifyToken, can('admin'), can('student'), reviewsRouter);
-app.use('/orders',verifyToken, can('admin'), can('student'), orderPaymentRouter);
-app.use('/webhook', webhookRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/courses", coursesRouter);
+app.use("/media", verifyToken, can("admin", "student"), mediaRouter);
+app.use("/refresh-tokens", refreshTokenRouter);
+app.use("/mentors", verifyToken, can("admin"), mentorsRouter);
+app.use("/chapters", verifyToken, can("admin"), chaptersRouter);
+app.use("/lessons", verifyToken, can("admin"), lessonsRouter);
+app.use("/image-courses", verifyToken, can("admin"), imageCoursesRouter);
+app.use("/my-courses", verifyToken, can("admin", "student"), myCoursesRouter);
+app.use("/reviews", verifyToken, can("admin", "student"), reviewsRouter);
+app.use("/orders", verifyToken, can("admin", "student"), orderPaymentRouter);
+app.use("/webhook", webhookRouter);
 
 module.exports = app;
